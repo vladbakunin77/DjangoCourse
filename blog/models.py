e8c6f9afd45a7ca86ad5dba761128daa9c46723f
vladbakunin77 <generalbakunin@gmail.com>
2020-03-30 23:46:36 +0300

    Создали модель категорий

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    '''Модель категорий'''
+    name = models.CharField(verbose_name='Имя',max_length=100)
+    slug = models.SlugField("Url",max_length=100)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name= 'Категория'
+        verbose_name_plural='Категории'
